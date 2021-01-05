277b06c44ebec874f699a3b184ebf3e3260d76dd
hao <hao>
2021-01-05 18:04:42 +0800

    20210105

---
+++ src/views/Home.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="home">
+    <div class="head">
+      <div class="container">
+        <div class="top-nav">
+          <div class="top-logo">
+            三务公开便民服务平台
+          </div>
+          <div class="top-ul">
+            <ul>
+              <li><a href="#">首页</a></li>
+              <li><a href="#">首页</a></li>
+              <li><a href="#">首页</a></li>
+              <li><a href="#">首页</a></li>
+              <li><a href="#">首页</a></li>
+              <li><a href="#">首页</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="search-input"></div>
+      </div>
+    </div>
+    <div class="container">
+      <!-- 最新公告 -->
+      <div class="content-box">
+        <div class="content-title">最新公告</div>
+        <div>The latest announcemen</div>
+        <div>
+          <div class=""></div>
+          <div>闵行区召开区委党的建设工作领导小组专题会议</div>
+          <div>组织部 2020-10-17</div>
+          <div>11月10日上午，闵行区召开区委党的建设工作领导小组专题会议。
+            区委书记、区委党的建设工作领导小组组长倪耀明主持会议并讲话。区委副书记、组织部部长王观…宝，区委常委、纪委书记、监察委主任李忠兴，区委常委、宣传部部长胡明华等区委党的建设工作领导小组副组长以及成员出席会议。
+            会议指出，2020年全区党的建设工作始终把政治建设摆在首位，坚决扛起了管党治党的政治责任，助推了重点任务，服务了中心大局，进一步筑牢了各级党组织和广大党员干部的思想之基，发挥了各级党组织的战斗堡垒作用和广大党员干部的先锋模范作用，推进了全面从严治党向纵深发展。
+            会议强调，要一以贯之把党的政治建设摆在首位，坚决抓好巡视整改，深入推进“四史”学习教育，全面加强思想政治建设；要一以贯之强化党建引领，始终围绕中心大局，提升党建工作实效，贯彻好新时代党的组织路线，持续提升组织力；要一以贯之推动全面从严治党向纵深发展，进一步落实全面从严治党主体责任，深化细化“四责协同”机制，以钉钉子精神打好作风建设持久战。
+            会议要求，要进一步落实习近平总书记对上海重要指示精神，按照中央、市委全面从严治党的工作要求，以政治建设为统领，以巡视整改为契机，不折不扣抓好各项工作任务落地见效，不断加强党建工作合力，助力“十三五”圆满收官，为顺利开启“十四五”发展新阶段奠定扎实基础。
+            会议审议通过《中共闵行区委党的建设工作领导小组办公室履行抓全面从严治党职责的工作规则》，研究讨论《关于聚焦闵行南虹桥推进枢纽党建，助力打造虹桥国际开放枢纽的实施意见》。
+          </div>
+        </div>
+      </div>
+      <!-- 快捷功能 -->
+      <div class="content-box">
+        <div class="content-title">快捷功能</div>
+        <div>Quick function</div>
+        <div>
+          <div>
+            <div class="item">
+              <div class="icon"></div>
+              <div>组织结构</div>
+              <div class="icon"></div>
+            </div>
+            <div class="item">
+              <div class="icon"></div>
+              <div>组织结构</div>
+              <div class="icon"></div>
+            </div>
+            <div class="item">
+              <div class="icon"></div>
+              <div>组织结构</div>
+              <div class="icon"></div>
+            </div>
+          </div>
+
+          <div>
+            <div class="left">
+              <ul>
+                <li>部门</li>
+                <li>部门</li>
+                <li>部门</li>
+                <li>部门</li>
+              </ul>
+            </div>
+            <div class="right">
+              <div>
+                <div>佛山市</div>
+                <div class="search-input"></div>
+              </div>
+              <div>
+                <div class="item">
+                  <ul>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                  </ul>
+                </div>
+                <div class="item">
+                  <ul>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                  </ul>
+                </div>
+                <div class="item">
+                  <ul>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                  </ul>
+                </div>
+                <div class="item">
+                  <ul>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                  </ul>
+                </div>
+                <div class="item">
+                  <ul>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                  </ul>
+                </div>
+                <div class="item">
+                  <ul>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                    <li>市发展改革委</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- 专题焦点 -->
+      <div class="content-box">
+        <div class="content-title">专题焦点</div>
+        <div>Special focus</div>
+        <div>
+          <div class="item"></div>
+          <div class="item"></div>
+          <div class="item"></div>
+          <div class="item"></div>
+        </div>
+      </div>
+      <div class="content-box">
+        <div class="item">
+          <div>网站信息</div>
+          <div>网站信息</div>
+          <div>网站信息</div>
+          <div>网站信息</div>
+        </div>
+        <div class="item">
+          <div>网站信息</div>
+          <div>网站信息</div>
+          <div>网站信息</div>
+          <div>网站信息</div>
+        </div>
+        <div class="item">
+          <div>网站信息</div>
+          <div>网站信息</div>
+          <div>网站信息</div>
+          <div>网站信息</div>
+        </div>
+      </div>
+    </div>
+    <div class="foot">
+      <div class="content-box">
+        三务公开便民服务平台
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
